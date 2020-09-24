1834ab6fcb6d7f58e5e47153dfffbf2a527746dd
Jessica Bradham <jessijoke@yahoo.com>
2020-09-24 03:27:25 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -20,8 +20,13 @@ function pressIt() {
 }
 
 function submitIt() {
-    $('img').on('load', function() {
-      $('img').addClass("tasty");
+    $("form").on("submit", function() {
+      if ($( "input:first" ).val() === "correct") {
+        alert('your form is going to be submitted now');
+        return;
+      }
+      alert("you entered the wrong value");
+      return;
     });
 }
 
